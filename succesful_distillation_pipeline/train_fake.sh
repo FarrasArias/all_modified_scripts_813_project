d535d3278f9758ea9efa36f5dec63b5f2ccf92f5
Rafael Arias González <rafael.arias.glez@gmail.com>
2022-12-14 22:45:22 -0800

    Update train_fake.sh

--- succesful_distillation_pipeline/train_fake.sh
+++ succesful_distillation_pipeline/train_fake.sh
@@ -8,14 +8,8 @@
 #SBATCH --mail-user raa60@sfu.ca
 #SBATCH --mail-type ALL
 
-
-echo "START"
 source $SCRATCH/mmm_training_with_distilgpt_011222/env_1024/bin/activate
-echo "START2"
 module load StdEnv/2020 protobuf python/3.6.10
-echo "START3"
 source $SCRATCH/mmm_training_with_distilgpt_011222/env_1024/bin/activate
-echo "START4"
 cd $SCRATCH/mmm_training_with_distilgpt_011222/MMM_TRAINING-master/
-echo "START5"
 python train.py --arch gpt2 --config config/distilgpt2.json --encoding EL_VELOCITY_DURATION_POLYPHONY_ENCODER --ngpu 4 --dataset /home/raa60/scratch/mmm_training_with_distilgpt_011222/data_v2_NUM_BARS\=4_OPZ_False.arr --batch_size 32 --label GENRE_DISCOGS --num_bars 4 --accum_steps 1 --ckpt ""
